12f060183e5ef3f7d08704d582d20b783bef3ff9
RicardoSantom <richard1978_1@hotmail.com>
2023-01-06 11:46:10 +0100

    Commit inicialización repositorio

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="Ricardo Santiago Tomé">
+        <link rel="stylesheet" href="webroot/css/estilos.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet">
+        <link rel="icon" type="image/png" sizes="96x96" href="../webroot/images/favicon-96x96.png">
+        <title>Index Desarrollo Interfaces Web</title>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Interfaces Web</h1>
+            <h2>Ejercicios prácticos</h2>
+        </header>
+        <main>
+            <article>
+                <h3>Enlaces a prácticas y enunciados.pdf</h3>
+                <ul>
+                    <li>
+                        <a id="practicas1">Prácticas 1ª evaluación</a>
+                        <hr>
+                        <ol>
+                            <li><a href="practica1.html">Práctica1</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio1.html">Ejercicio 1</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio2.html">Ejercicio 2</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio3.html">Ejercicio 3</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio4.html">Ejercicio 4</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio5.html">Ejercicio 5</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio6.html">Ejercicio 6</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio7.html">Ejercicio 7</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio8.html">Ejercicio 8</a></li>
+                            <li><a href="codigoJS/practicas1/ejercicio9.html">Ejercicio 9</a></li>
+                        </ol>
+                    </li>
+                </ul>
+            </article>
+        </main>
+         <footer>
+            <p>Ricardo Santiago Tomé © Todos los derechos reservados</p>
+            <a href="https://github.com/RicardoSantom" target="blank" id="github" title="RicardoSantom en GitHub"></a>
+            <a href="https://www.linkedin.com/in/ricardo-santiago-tom%C3%A9/" id="linkedin" title="Ricardo Santiago Tomé en Linkedim" target="_blank"></a>
+            <a href="doc/curriculumRicardo.pdf" class="material-icons" title="Curriculum Vitae Ricardo Santiago Tomé" target="_blank" id="curriculum"><span class="material-icons md-18">face</span></a>
+            <p>
+                <!--<a href="http://jigsaw.w3.org/css-validator/check/referer">-->
+                    <img style="border:0;width:84px;height:27px"
+                         src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                         alt="¡CSS Válido!">
+                <!--</a>-->
+            </p>
+        </footer>
+    </body>
+</html>
