2846bf1ad261c6f49aba098934f4883f1c5009bc
RicardoSantom <richard1978_1@hotmail.com>
2023-01-06 13:04:16 +0100

    Primera versión estable

--- index.php
+++ index.php
@@ -19,40 +19,52 @@
             <h2>Ejercicios prácticos</h2>
         </header>
         <main>
-            <article>
-                <h3>Enlaces a prácticas y enunciados.pdf</h3>
-                <ul>
-                    <li>
-                        <a id="practicas1">Prácticas 1ª evaluación</a>
-                        <hr>
-                        <ol>
-                            <li><a href="practica1.html">Práctica1</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio1.html">Ejercicio 1</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio2.html">Ejercicio 2</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio3.html">Ejercicio 3</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio4.html">Ejercicio 4</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio5.html">Ejercicio 5</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio6.html">Ejercicio 6</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio7.html">Ejercicio 7</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio8.html">Ejercicio 8</a></li>
-                            <li><a href="codigoJS/practicas1/ejercicio9.html">Ejercicio 9</a></li>
-                        </ol>
-                    </li>
-                </ul>
-            </article>
+            <div class="enlaces">
+            <ul>
+                <a id="practicas1">Prácticas Tema1</a>
+                <hr>
+                <li><a href="practica1.html">Práctica1</a></li>
+                <li><a href="1.2-2.html">Práctica 1.2</a></li>
+                <li><a href="1-2-3.html">Práctica 1.3</a></li>
+                <li><a href="1-4.html">Práctica 1.4</a></li>
+            </ul>
+            <ul>
+                <a id="practicas2">Prácticas Tema2</a>
+                <hr>
+                <li><a href="practica2.html">Práctica 2</a></li>
+                <li><a href="practica2-2.html">Práctica 2.2</a></li>
+                <li><a href="practica2-3.html">Práctica 2.3</a></li>
+                <li><a href="practica2-4-1.html">Práctica 2.4.1</a></li>
+                <li><a href="practica2-4-2.html">Práctica 2.4.2</a></li>
+                <li><a href="practica2-4-3.html">Práctica 2.4.3</a></li>
+                <li><a href="practica2-5.html">Práctica 2.5</a></li>
+                <li><a href="practica2-6.html">Práctica 2.6</a></li>
+            </ul>
+            <ul>
+                <a id="practicas3">Prácticas Tema2</a>
+                <hr>
+                <li><a href="practica2-8-1.html">Práctica 2.8.1</a></li>
+                <li><a href="practica2-8-2-a.html">Práctica 2.8.2.a</a></li>
+                <li><a href="practica2-8-2-b.html">Práctica 2.8.2.b</a></li>
+                <li><a href="practica2-8-3.html">Práctica 2.8.3</a></li>
+                <li><a href="practica2-8-6.html">Práctica 2.8.6</a></li>
+                <li><a href="practica-pruebaAmazon.html">Práctica prueba Amazon</a></li>
+            </ul>
+            </div>
         </main>
-         <footer>
+        <footer>
             <p>Ricardo Santiago Tomé © Todos los derechos reservados</p>
             <a href="https://github.com/RicardoSantom" target="blank" id="github" title="RicardoSantom en GitHub"></a>
             <a href="https://www.linkedin.com/in/ricardo-santiago-tom%C3%A9/" id="linkedin" title="Ricardo Santiago Tomé en Linkedim" target="_blank"></a>
             <a href="doc/curriculumRicardo.pdf" class="material-icons" title="Curriculum Vitae Ricardo Santiago Tomé" target="_blank" id="curriculum"><span class="material-icons md-18">face</span></a>
             <p>
                 <!--<a href="http://jigsaw.w3.org/css-validator/check/referer">-->
-                    <img style="border:0;width:84px;height:27px"
-                         src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-                         alt="¡CSS Válido!">
+                <img style="border:0;width:84px;height:27px"
+                     src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+                     alt="¡CSS Válido!">
                 <!--</a>-->
             </p>
+            <a href="http://daw208.ieslossauces.es/index.html" id="enlacePrincipal" title="Enlace a Index Principal">Página principal</a>
         </footer>
     </body>
 </html>
